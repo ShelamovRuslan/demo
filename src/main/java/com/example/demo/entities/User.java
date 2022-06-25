56e7d41df86bf88ce055b67005cd0bb54c1882c7
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-06-25 04:44:17 +0300

    2

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,47 @@
+package com.example.demo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "login", nullable = false, length = 200)
+    private String login;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
